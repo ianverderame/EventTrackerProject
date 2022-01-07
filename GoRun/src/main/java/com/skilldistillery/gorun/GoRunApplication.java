e268b1264ee8081892f7ecee9a96e12be97edd0c
Ian Verderame <ianverderame9@gmail.com>
2022-01-07 15:40:34 -0700

    Create Spring Boot project

---
+++ GoRun/src/main/java/com/skilldistillery/gorun/GoRunApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.gorun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRunApplication.class, args);
+	}
+
+}
